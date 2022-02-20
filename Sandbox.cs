7ad1b8f0594283553c3437c181cd7decc55c8dc9
Nicks <1313nicov@gmail.com>
2022-02-20 10:41:58 -0500

    División de los ejercicios
    Comit de prueba, separando cada función según el punto de los ejercicios a los caules pertenencen

--- Sandbox.cs
+++ Sandbox.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 
 namespace TestProject1
 {
@@ -10,4 +10,19 @@ namespace TestProject1
             Console.WriteLine("Here you can write console prints to test your implementation outside the testing environment");
         }
     }
+
+
+    //Ejercicio 1:
+    internal static Stack<int> GetNextGreaterValue(Stack<int> sourceStack)
+    {
+        Stack<int> result = new Stack<int>(); //Crea la pila para los resultados
+
+        While(sourceStack()! = null) {
+            Int dato = sourceStack.pop();
+            Situar(dato, result, new Stack<int>());
+        }
+
+        return result;
+    }
+
 }
--- TestMethods.cs
+++ TestMethods.cs
@@ -4,6 +4,7 @@ namespace TestProject1
 {
     internal class TestMethods
     {
+        //Definición para el Ejercicio 2
         internal enum EValueType
         {
             Two,
@@ -13,13 +14,17 @@ namespace TestProject1
             Prime
         }
 
+
+        //Ejercicio 1:
         internal static Stack<int> GetNextGreaterValue(Stack<int> sourceStack)
         {
-            Stack<int> result = null;
+            Stack<int> result = new Stack<int>(); //Crea la pila para los resultados
+
 
             return result;
         }
 
+        //Ejercicio 2A
         internal static Dictionary<int, EValueType> FillDictionaryFromSource(int[] sourceArr)
         {
             Dictionary<int, EValueType> result = null;
@@ -27,11 +32,13 @@ namespace TestProject1
             return result;
         }
 
+        //Ejercicio 2B
         internal static int CountDictionaryRegistriesWithValueType(Dictionary<int, EValueType> sourceDict, EValueType type)
         {
             return 0;
         }
 
+        //Ejercicio 2C
         internal static Dictionary<int, EValueType> SortDictionaryRegistries(Dictionary<int, EValueType> sourceDict)
         {
             Dictionary<int, EValueType> result = null;
@@ -39,6 +46,7 @@ namespace TestProject1
             return result;
         }
 
+        //Ejercicio 3A
         internal static Queue<Ticket>[] ClassifyTickets(List<Ticket> sourceList)
         {
             Queue<Ticket>[] result = null;
@@ -46,6 +54,7 @@ namespace TestProject1
             return result;
         }
 
+        //Ejercicio 3B
         internal static bool AddNewTicket(Queue<Ticket> targetQueue, Ticket ticket)
         {
             bool result = false;
