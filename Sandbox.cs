3889156c7585a6f70d56665ff10d9d1122bed19e
Nicks <1313nicov@gmail.com>
2022-02-20 19:49:00 -0500

    Prueba02
    Segundo comit de prueba

--- Sandbox.cs
+++ Sandbox.cs
@@ -1,4 +1,5 @@
 ﻿ using System;
+ using System.Collections.Generic; //agrego esto para usar Sandbox de borrador
 
 namespace TestProject1
 {
@@ -15,14 +16,51 @@ namespace TestProject1
     //Ejercicio 1:
     internal static Stack<int> GetNextGreaterValue(Stack<int> sourceStack)
     {
-        Stack<int> result = new Stack<int>(); //Crea la pila para los resultados
+        //para evitarnos problemas, vamos a hacer un back up de la Pila Fuente:
+        int[] trasnferSourceToArray = sourceStack.ToArray(); //Trasferimos a un arreglo todo el contenido de sourceStack
 
-        While(sourceStack()! = null) {
-            Int dato = sourceStack.pop();
-            Situar(dato, result, new Stack<int>());
+        //Creamos las estructuras que vamos a utilizar
+        Stack<int> calkStack = new Stack<int>(); //Crea la pila en la que vamos a calcar a sourceStack
+        Stack<int> auxStack = new Stack<int>(); //Crea la pila Auxiliar
+        Stack<int> result = new Stack<int>(); //Crea la pila para los resultados
+        List<int> dataList = new List<int>(); //Crea una lista para ordenar los resultados 
+
+        //Llenamos la Pila calkStack con el contenido del Aregglo de Trasferencia
+        for (int i =trasnferSourceToArray.Length -1, i >= 0; i--)
+        {
+            calkStack.Push(trasnferSourceToArray[i]); 
         }
 
+        //Mientras en calkStack haya elemntos: 
+        while(calkStack()! = null) {
+
+            int dato = calkStack.pop();
+            int biggestData = dato; 
+            elementList.Add(dato);
+            ColocarEnPilas(dato,result,auxStack);
+            
+        }
+        
         return result;
     }
+
+    private void ColocarEnPilas (int dato, Stack<int> auxStack, List<int> elementList)
+    {
+
+        if (auxStack.peek() == null)
+        {
+            result.push(-1);
+            //luego tenemos que llevar todos los platos de auxStack a result
+        }
+        else
+        {
+            if (dato.compareTo(result.peek() < 0))
+            {
+
+            }
+        }
+    }
+
+
 
 }
--- TestMethods.cs
+++ TestMethods.cs
@@ -18,8 +18,31 @@ namespace TestProject1
         //Ejercicio 1:
         internal static Stack<int> GetNextGreaterValue(Stack<int> sourceStack)
         {
-            Stack<int> result = new Stack<int>(); //Crea la pila para los resultados
+            //para evitarnos problemas, vamos a hacer un back up de la Pila Fuente:
+            int[] trasnferSourceToArray = sourceStack.ToArray(); //Trasferimos a un arreglo todo el contenido de sourceStack
 
+            //Creamos las estructuras que vamos a utilizar
+            Stack<int> calkStack = new Stack<int>(); //Crea la pila en la que vamos a calcar a sourceStack
+            Stack<int> auxStack = new Stack<int>(); //Crea la pila Auxiliar
+            Stack<int> result = new Stack<int>(); //Crea la pila para los resultados
+            List<int> dataList = new List<int>(); //Crea una lista para ordenar los resultados 
+
+            //Llenamos la Pila calkStack con el contenido del Aregglo de Trasferencia
+            for (int i = trasnferSourceToArray.Length - 1, i >= 0; i--)
+            {
+                calkStack.Push(trasnferSourceToArray[i]);
+            }
+
+            //Mientras en calkStack haya elemntos: 
+            while (calkStack()! = null)
+            {
+
+                int dato = calkStack.pop();
+                int biggestData = dato;
+                elementList.Add(dato);
+                ColocarEnPilas(dato, result, auxStack);
+
+            }
 
             return result;
         }
